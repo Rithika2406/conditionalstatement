2cf64a40abc2a1127d92a33ef73d1bf403b47ce1
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-09 17:02:54 +0530

    Add files via upload

---
+++ conditionalstmt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() 
+{
+    int a = 13;
+    int b = 15;
+    if (a >= b)
+    {
+        printf("%d is greater than or equal to %d\n", a,b);
+    }   
+    else
+    {
+        printf("%d is lesser than %d\n", a,b);
+    }
+    
+    // lets add the else if statement
+    a = 14;
+    b = 14;
+    if(a > b)
+    {
+        printf("%d is greater than %d\n", a,b);
+    }
+    else if(a==b)
+    {
+        printf("%d is equal to %d\n", a,b);
+    }
+    else
+    {
+        printf("%d is lesser than %d\n", a,b);
+    }
+    
+    return 0;
+}
